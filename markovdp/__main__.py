c8a4f7e5ca2e5bb5888bafef26ff6ec31d673a75
snjot <snjot.dev@gmail.com>
2020-10-25 02:43:16 +0900

    :+1: add verbose mode

--- markovdp/__main__.py
+++ markovdp/__main__.py
@@ -14,9 +14,10 @@ def parse_args():
     parser.add_argument(
         "--delay",
         type=float,
-        default=0.5,
+        default=0.0,
         help="seconds to delay each step",
     )
+    parser.add_argument("--verbose", action="store_true")
     return parser.parse_args()
 
 
@@ -24,13 +25,14 @@ def create_grid() -> List[List[int]]:
     return [[0, 0, 0, 1], [0, 9, 0, -1], [0, 0, 0, 0]]
 
 
-def main(delay: float):
+def main(delay: float, is_verbose: bool):
     grid = create_grid()
     env = Environment(grid)
     agent = Agent(env)
 
     for i in range(N_GAMES):
-        print(f"-- GAME {i} START --")
+        if is_verbose:
+            print(f"-- GAME {i} START --")
         state = env.reset()
         total_reward = 0.0
         done = False
@@ -39,21 +41,22 @@ def main(delay: float):
 
         while not done:
             action = agent.policy(state)
-            print(f"Action: {action}")
-
             next_state, reward, done = env.step(action)
             time.sleep(delay)
-            print(f"State: {next_state}")
+            if is_verbose:
+                print(f"Action: {action}")
+                print(f"State: {next_state}")
 
             total_reward += reward
             state = next_state
 
             state_history.append(state)
 
-        print(f"History: {state_history}")
+        if is_verbose:
+            print(f"History: {state_history}")
         print(f"Episode {i}: Agent gets {total_reward} reward.")
 
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.delay)
+    main(args.delay, args.verbose)
